41fa2306ceda872504d1c0502ee05a1c5222f71c
Jean Rojas <jean.rojas@usach.cl>
2024-12-11 00:54:26 -0300

    Cambiando conexion.js

--- Backend/bd/conexion.js
+++ Backend/bd/conexion.js
@@ -3,7 +3,7 @@ const {Pool} = require('pg');
 const pool = new Pool({
     host:'localhost',
     user: 'postgres',
-    password:'1243',
+    password:'postgres',
     database: 'likeme',
     allowExitOnIdle:true
 }); 
